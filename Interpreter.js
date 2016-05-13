ed6223966a46f9c56d585d4d70d7de7e295ff0c8
mbx-uni-aka <mailbox.endless@gmail.com>
2016-05-13 10:40:53 +0200

    merged

--- Interpreter.js
+++ Interpreter.js
@@ -1,92 +1,5 @@
-// Interface definitions for worlds
-///<reference path="World.ts"/>
-///<reference path="lib/node.d.ts"/>
-/**
-* Parser module
-*
-* This module parses a command given as a string by the user into a
-* list of possible parses, each of which contains an object of type
-* `Command`.
-*
-*/
-var Parser;
-(function (Parser) {
-    //////////////////////////////////////////////////////////////////////
-    // exported functions, classes and interfaces/types
-    function parse(input) {
-        var nearleyParser = new nearley.Parser(grammar.ParserRules, grammar.ParserStart);
-        var parsestr = input.toLowerCase().replace(/\W/g, "");
-        try {
-            var results = nearleyParser.feed(parsestr).results;
-        }
-        catch (err) {
-            if ('offset' in err) {
-                throw new Error('Parsing failed after ' + err.offset + ' characters');
-            }
-            else {
-                throw err;
-            }
-        }
-        if (!results.length) {
-            throw new Error('Parsing failed, incomplete input');
-        }
-        return results.map(function (res) {
-            // We need to clone the parse result, because parts of it is shared with other parses
-            return { input: input, parse: clone(res) };
-        });
-    }
-    Parser.parse = parse;
-    function stringify(result) {
-        return JSON.stringify(result.parse);
-    }
-    Parser.stringify = stringify;
-    //////////////////////////////////////////////////////////////////////
-    // Utilities
-    function clone(obj) {
-        return JSON.parse(JSON.stringify(obj));
-    }
-})(Parser || (Parser = {}));
-if (typeof require !== 'undefined') {
-    // Node.JS way of importing external modules
-    // In a browser, they must be included from the HTML file
-    var nearley = require('./lib/nearley.js');
-    var grammar = require('./grammar.js');
-}
-///<reference path="World.ts"/>
-///<reference path="Parser.ts"/>
-/**
-* Interpreter module
-*
-* The goal of the Interpreter module is to interpret a sentence
-* written by the user in the context of the current world state. In
-* particular, it must figure out which objects in the world,
-* i.e. which elements in the `objects` field of WorldState, correspond
-* to the ones referred to in the sentence.
-*
-* Moreover, it has to derive what the intended goal state is and
-* return it as a logical formula described in terms of literals, where
-* each literal represents a relation among objects that should
-* hold. For example, assuming a world state where "a" is a ball and
-* "b" is a table, the command "put the ball on the table" can be
-* interpreted as the literal ontop(a,b). More complex goals can be
-* written using conjunctions and disjunctions of these literals.
-*
-* In general, the module can take a list of possible parses and return
-* a list of possible interpretations, but the code to handle this has
-* already been written for you. The only part you need to implement is
-* the core interpretation function, namely `interpretCommand`, which produces a
-* single interpretation for a single command.
-*/
 var Interpreter;
 (function (Interpreter) {
-    //////////////////////////////////////////////////////////////////////
-    // exported functions, classes and interfaces/types
-    /**
-    Top-level function for the Interpreter. It calls `interpretCommand` for each possible parse of the command. No need to change this one.
-    * @param parses List of parses produced by the Parser.
-    * @param currentState The current state of the world.
-    * @returns Augments ParseResult with a list of interpretations. Each interpretation is represented by a list of Literals.
-    */
     function interpret(parses, currentState) {
         var errors = [];
         var interpretations = [];
@@ -104,7 +17,6 @@ var Interpreter;
             return interpretations;
         }
         else {
-            // only throw the first error found
             throw errors[0];
         }
     }
@@ -112,7 +24,6 @@ var Interpreter;
     function stringify(result) {
         return result.interpretation.map(function (literals) {
             return literals.map(function (lit) { return stringifyLiteral(lit); }).join(" & ");
-            // return literals.map(stringifyLiteral).join(" & ");
         }).join(" | ");
     }
     Interpreter.stringify = stringify;
@@ -120,34 +31,22 @@ var Interpreter;
         return (lit.polarity ? "" : "-") + lit.relation + "(" + lit.args.join(",") + ")";
     }
     Interpreter.stringifyLiteral = stringifyLiteral;
-    //////////////////////////////////////////////////////////////////////
-    // private functions
-    /**
-     * The core interpretation function. The code here is just a
-     * template; you should rewrite this function entirely. In this
-     * template, the code produces a dummy interpretation which is not
-     * connected to `cmd`, but your version of the function should
-     * analyse cmd in order to figure out what interpretation to
-     * return.
-     * @param cmd The actual command. Note that it is *not* a string, but rather an object of type `Command` (as it has been parsed by the parser).
-     * @param state The current state of the world. Useful to look up objects in the world.
-     * @returns A list of list of Literal, representing a formula in disjunctive normal form (disjunction of conjunctions). See the dummy interpetation returned in the code for an example, which means ontop(a,floor) AND holding(b).
-     */
     function interpretCommand(cmd, state) {
-        // This returns a dummy interpretation involving two random objects in the world
-        var objects = Array.prototype.concat.apply([], state.stacks);
-        var a = objects[Math.floor(Math.random() * objects.length)];
-        var b = objects[Math.floor(Math.random() * objects.length)];
-        var interpretation = [[
-                { polarity: true, relation: "ontop", args: [a, "floor"] },
-                { polarity: true, relation: "holding", args: [b] }
-            ]];
+        var interpretation;
+        var entity = interpretEntity(cmd.entity, state);
+        var location = interpretLocation(cmd.location, state);
+        if (cmd.command == "take") {
+        }
+        else if (cmd.command == "put") {
+        }
+        else if (cmd.command == "move") {
+        }
+        else {
+        }
         return interpretation;
     }
     function interpretObject(obj, state) {
-        // returns the list of strings representing the relevant objects
         var res = [];
-        // TODO : refactor this case, try to make it less copy-pasty!
         if (obj.form) {
             var obList = state.objects;
             var f = obj.form;
@@ -184,26 +83,61 @@ var Interpreter;
                 }
             }
         }
-        // Handle objects with relative definitions recursively, using
-        // row and col coordinates
+        else if (obj.location) {
+            var possible = interpretObject(obj.object, state);
+            var pLocations = interpretLocation(obj.location, state);
+            for (var _i = 0, possible_1 = possible; _i < possible_1.length; _i++) {
+                var candidate = possible_1[_i];
+                for (var _a = 0, pLocations_1 = pLocations; _a < pLocations_1.length; _a++) {
+                    var l = pLocations_1[_a];
+                    switch (l.rel) {
+                        case "inside":
+                            candidate = null;
+                            break;
+                        case "above":
+                            candidate = null;
+                            break;
+                        case "beside":
+                            candidate = null;
+                            break;
+                        case "ontop":
+                            var stacks = state.stacks;
+                            for (var _b = 0, stacks_1 = stacks; _b < stacks_1.length; _b++) {
+                                var currStack = stacks_1[_b];
+                                var candidatePosition = currStack.indexOf(candidate);
+                                var objectPosition = currStack.indexOf(l.id);
+                                if (objectPosition < 0)
+                                    break;
+                                if (candidatePosition == objectPosition + 1) {
+                                    res.push(candidate);
+                                }
+                            }
+                            break;
+                        case "leftof":
+                            candidate = null;
+                            break;
+                        case "rightof":
+                            candidate = null;
+                            break;
+                    }
+                }
+            }
+        }
         return res;
     }
     function interpretEntity(ent, state) {
-        // calls interpretObject, more complex quantifier handling can be added later
-        return;
-    }
-    function interpretLocation(loc, state) {
-        var res = { locations: [] };
+        var res = [];
         return res;
     }
-    var ObjectInfo = (function () {
-        function ObjectInfo() {
+    function interpretLocation(loc, state) {
+        var res = [];
+        var rel = loc.relation;
+        var ent = loc.entity;
+        var candidates = interpretEntity(ent, state);
+        for (var _i = 0, candidates_1 = candidates; _i < candidates_1.length; _i++) {
+            var candidate = candidates_1[_i];
+            res.push({ rel: rel, id: candidate });
         }
-        return ObjectInfo;
-    }());
-    var LocationInfo = (function () {
-        function LocationInfo() {
-        }
-        return LocationInfo;
-    }());
+        return res;
+    }
 })(Interpreter || (Interpreter = {}));
