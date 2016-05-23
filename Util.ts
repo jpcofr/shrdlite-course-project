b882502f044a7d92ebab6a652fe103ef343a34a2
mbx-uni-aka <endless_mailbox@yahoo.it>
2016-05-23 03:47:55 +0200

    Util fixed

--- Util.ts
+++ Util.ts
@@ -1,24 +1,25 @@
 /*** General purpose utilites ***/
 
+// A function to clone arrays.
+function cloneArray (array : any[]) : any[] {
+    var newArray = [];
+    for(var obj of array) {newArray.push(obj);}
+
+    return newArray;
+}
+
 // A function to clone matrices.
 function cloneMatrix (matrix : any[][]) : any[][] {
     var newMatrix = [];
-
-    for(var row of matrix) {
-      var newRow = [];
-        for(var obj of row) {
-          newRow.push(obj);
-        }
-      newMatrix.push(newRow);
-    }
+    for(var row of matrix) {newMatrix.push(cloneArray(row));}
 
     return newMatrix;
 }
 
 // Computes the sorted, duplicate-free version of a list.
 function uniqueSort(xs : any[], cmp : (x:any, y:any) => number) : any[] {
-   var sorted : any[] = xs.sort(cmp);
-   var result : any[] = [];
+   var sorted = cloneArray(xs).sort(cmp);
+   var result = [];
 
    for(var i = 0; i < sorted.length - 1; i++)
        if(cmp(sorted[i], sorted[i + 1]) != 0)
@@ -32,14 +33,15 @@ function uniqueSort(xs : any[], cmp : (x:any, y:any) => number) : any[] {
 
 // Computes the intersection of two lists.
 function intersect(xs : any[], ys : any[], cmp : (x:any, y:any) => number) {
-   var sorted1 : any[] = xs.sort(cmp);
-   var sorted2 : any[] = ys.sort(cmp);
-   var result  : any[] = [];
+   var sorted1 = cloneArray(xs).sort(cmp);
+   var sorted2 = cloneArray(ys).sort(cmp);
+   var result  = [];
 
    for(var i = 0, j = 0; i < sorted1.length && j < sorted2.length;) {
        var r = cmp(sorted1[i], sorted2[j]);
+
        if (r == 0) {result.push(sorted1[i]); i++; j++;}
-       else {r == -1 ? i++ : j++;}
+       else        {r == -1 ? i++ : j++;}
    }
 
    return result;
